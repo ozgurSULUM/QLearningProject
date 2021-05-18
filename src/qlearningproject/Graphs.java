ec723a23e2bb5a7eeb74c8c69920e9ff26f4259d
Ozgur Sulum <ozgursulum0@gmail.com>
2021-05-18 22:35:43 +0300

    latest version

--- src/qlearningproject/Graphs.java
+++ src/qlearningproject/Graphs.java
@@ -5,21 +5,15 @@
  */
 package qlearningproject;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.RenderingHints;
-import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
-import javafx.stage.Stage;  
 /**
  *
  * @author ozgur
@@ -31,6 +25,12 @@ public class Graphs extends javax.swing.JFrame {
      */
     private static final int BORDER_GAP = 5;
     private static final Color GRAPH_COLOR = Color.green;
+    private final int padding = 25;
+    private final int labelPadding = 25;
+    private final Color lineColor = new Color(44, 102, 230, 180);
+    private final Color gridColor = new Color(200, 200, 200, 200);
+    private final int pointWidth = 4;
+    private final int numberYDivisions = 10;
     
     private ArrayList<Integer> costs_per_episode;
     private ArrayList<Integer> steps_per_episode;
@@ -42,7 +42,6 @@ public class Graphs extends javax.swing.JFrame {
     public void createGraph(JPanel panel,ArrayList<Integer> scores){
         Graphics2D g2 = (Graphics2D)panel.getGraphics();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        
         double max_score = scores.get(0);
         double min_score = scores.get(0);
         for(Integer score:scores){
@@ -53,31 +52,80 @@ public class Graphs extends javax.swing.JFrame {
                 min_score = score;
             }
         }
-        System.out.println("width:"+panel.getWidth()+" height:"+panel.getHeight());
-        double score_size = max_score - min_score;
-        double xScale = ((double) panel.getWidth() - 2*BORDER_GAP) / (scores.size() - 1);
-        double yScale = ((double) panel.getHeight() - 3*BORDER_GAP) / (score_size-1);
-        
-        ArrayList<Point> graphPoints = new ArrayList<Point>();
+        double xScale = ((double) panel.getWidth() - (2 * padding) - labelPadding) / (scores.size() - 1);
+        double yScale = ((double) panel.getHeight() - 2 * padding - labelPadding) / (max_score - min_score);
+
+        ArrayList<Point> graphPoints = new ArrayList<>();
         for (int i = 0; i < scores.size(); i++) {
-         int x1 = (int) ((i * xScale) + BORDER_GAP+1);
-         int y1 = (int) (((score_size - scores.get(i)) * yScale) - BORDER_GAP-5);
-         graphPoints.add(new Point(x1, y1));
+            int x1 = (int) (i * xScale + padding + labelPadding);
+            int y1 = (int) ((max_score - scores.get(i)) * yScale + padding);
+            graphPoints.add(new Point(x1, y1));
+        }
+
+        // draw white background
+        g2.setColor(Color.WHITE);
+        g2.fillRect(padding + labelPadding, padding, panel.getWidth() - (2 * padding) - labelPadding, panel.getHeight() - 2 * padding - labelPadding);
+        g2.setColor(Color.BLACK);
+
+        // create hatch marks and grid lines for y axis.
+        for (int i = 0; i < numberYDivisions + 1; i++) {
+            int x0 = padding + labelPadding;
+            int x1 = pointWidth + padding + labelPadding;
+            int y0 = panel.getHeight() - ((i * (panel.getHeight() - padding * 2 - labelPadding)) / numberYDivisions + padding + labelPadding);
+            int y1 = y0;
+            if (scores.size() > 0) {
+                g2.setColor(gridColor);
+                g2.drawLine(padding + labelPadding + 1 + pointWidth, y0, panel.getWidth() - padding, y1);
+                g2.setColor(Color.BLACK);
+                String yLabel = ((int) ((min_score + (max_score - min_score) * ((i * 1.0) / numberYDivisions)) * 100)) / 100.0 + "";
+                FontMetrics metrics = g2.getFontMetrics();
+                int labelWidth = metrics.stringWidth(yLabel);
+                g2.drawString(yLabel, x0 - labelWidth - 5, y0 + (metrics.getHeight() / 2) - 3);
+            }
+            g2.drawLine(x0, y0, x1, y1);
+        }
+
+        // and for x axis
+        AffineTransform default_at = g2.getTransform();
+       
+        
+        for (int i = 0; i < scores.size(); i++) {
+            if (scores.size() > 1) {
+                int x0 = i * (panel.getWidth() - padding * 2 - labelPadding) / (scores.size() - 1) + padding + labelPadding;
+                int x1 = x0;
+                int y0 = panel.getHeight() - padding - labelPadding;
+                int y1 = y0 - pointWidth;
+                if ((i % ((int) ((scores.size() / 20.0)) + 1)) == 0) {
+                    g2.setColor(gridColor);
+                    g2.drawLine(x0, panel.getHeight() - padding - labelPadding - 1 - pointWidth, x1, padding);
+                    g2.setColor(Color.BLACK);
+                    String xLabel = i + "";
+                    FontMetrics metrics = g2.getFontMetrics();
+                    int labelWidth = metrics.stringWidth(xLabel);
+                    g2.rotate(Math.toRadians(90),x0 - labelWidth / 2 ,y0 + metrics.getHeight() + 3);
+                    g2.translate(0,-10);
+                    g2.drawString(xLabel,x0 - labelWidth / 2 ,y0 + metrics.getHeight() + 3 );
+                    g2.setTransform(default_at);
+                }
+            }
         }
         
         // create x and y axes 
-        g2.drawLine(BORDER_GAP, panel.getHeight() - BORDER_GAP, BORDER_GAP, BORDER_GAP);
-        g2.drawLine(BORDER_GAP, panel.getHeight() - BORDER_GAP, panel.getWidth() - BORDER_GAP, panel.getHeight() - BORDER_GAP);
+        g2.drawLine(padding + labelPadding, panel.getHeight() - padding - labelPadding, padding + labelPadding, padding);
+        g2.drawLine(padding + labelPadding, panel.getHeight() - padding - labelPadding, panel.getWidth() - padding, panel.getHeight() - padding - labelPadding);
+
         
-        g2.setColor(GRAPH_COLOR);
+        g2.setColor(lineColor);
         for (int i = 0; i < graphPoints.size() - 1; i++) {
-           int x1 = graphPoints.get(i).x;
-           int y1 = graphPoints.get(i).y;
-           int x2 = graphPoints.get(i + 1).x;
-           int y2 = graphPoints.get(i + 1).y;
-           g2.drawLine(x1, y1, x2, y2);         
+            int x1 = graphPoints.get(i).x;
+            int y1 = graphPoints.get(i).y;
+            int x2 = graphPoints.get(i + 1).x;
+            int y2 = graphPoints.get(i + 1).y;
+            g2.drawLine(x1, y1, x2, y2);
         }
 
+        
+        
     }
     
     /**
@@ -97,40 +145,42 @@ public class Graphs extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1250, 610));
+
+        jPanel1.setPreferredSize(new java.awt.Dimension(1250, 610));
 
         jCostPanel.setBackground(new java.awt.Color(204, 204, 204));
         jCostPanel.setToolTipText("");
-        jCostPanel.setMaximumSize(new java.awt.Dimension(500, 500));
-        jCostPanel.setMinimumSize(new java.awt.Dimension(500, 500));
-        jCostPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+        jCostPanel.setMaximumSize(new java.awt.Dimension(600, 500));
+        jCostPanel.setMinimumSize(new java.awt.Dimension(600, 500));
 
         javax.swing.GroupLayout jCostPanelLayout = new javax.swing.GroupLayout(jCostPanel);
         jCostPanel.setLayout(jCostPanelLayout);
         jCostPanelLayout.setHorizontalGroup(
             jCostPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jCostPanelLayout.setVerticalGroup(
             jCostPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 500, Short.MAX_VALUE)
         );
 
         jStepPanel.setBackground(new java.awt.Color(204, 204, 204));
         jStepPanel.setForeground(new java.awt.Color(255, 255, 255));
         jStepPanel.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        jStepPanel.setMaximumSize(new java.awt.Dimension(500, 500));
-        jStepPanel.setMinimumSize(new java.awt.Dimension(500, 500));
-        jStepPanel.setPreferredSize(new java.awt.Dimension(500, 500));
+        jStepPanel.setMaximumSize(new java.awt.Dimension(600, 500));
+        jStepPanel.setMinimumSize(new java.awt.Dimension(600, 500));
+        jStepPanel.setPreferredSize(new java.awt.Dimension(600, 500));
 
         javax.swing.GroupLayout jStepPanelLayout = new javax.swing.GroupLayout(jStepPanel);
         jStepPanel.setLayout(jStepPanelLayout);
         jStepPanelLayout.setHorizontalGroup(
             jStepPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jStepPanelLayout.setVerticalGroup(
             jStepPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         jLabel1.setFont(new java.awt.Font("Ubuntu Mono", 0, 24)); // NOI18N
@@ -153,21 +203,20 @@ public class Graphs extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 450, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jCostPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 581, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jStepPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(523, 523, 523)
-                .addComponent(jButton1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jCostPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jStepPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(588, 588, 588)
+                        .addComponent(jButton1)))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -178,18 +227,18 @@ public class Graphs extends javax.swing.JFrame {
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jStepPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jCostPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(jCostPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jStepPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addContainerGap())
+                .addGap(34, 34, 34))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1252, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
--- src/qlearningproject/QLearningProject.java
+++ src/qlearningproject/QLearningProject.java
@@ -2,18 +2,21 @@
 package qlearningproject;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 
 
 public class QLearningProject {
 
     public static void main(String[] args) {
+        
         Window window =  new Window();
         window.setVisible(true);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setFocusable(true);
+        
         
     }
     
